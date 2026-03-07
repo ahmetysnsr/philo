f5f27c480cc6c5d1056fcaf32228e1bf345283fd
ahmetysnsr <dude@DESKTOP-GLG8FCE.localdomain>
2026-03-07 16:08:04 +0300

    fix:start_philos

--- init_functions/start_philos.c
+++ init_functions/start_philos.c
@@ -1,7 +1,7 @@
 ﻿
 
 #include <pthread.h>
-
+#include <stdio.h>
 #include "../philo.h"
 
 void	start_philos(t_data *data)
@@ -11,14 +11,17 @@ void	start_philos(t_data *data)
 	i = 0;
 	while (i < data->number_of_philosophers)
 	{
-		if (pthread_create(&data->philosophers[i].thread, NULL, philo_routine, &data->philosophers[i]))
-			return;
+		if (pthread_create(&data->philosophers[i].thread, NULL,
+				philo_routine, &data->philosophers[i]) != 0)
+		{
+			pthread_mutex_lock(&data->write_lock);
+			data->stop_flag = 1;
+			pthread_mutex_unlock(&data->write_lock);
+			printf("Error: Failed to create thread for philosopher %d\n", i + 1);
+			break ;
+		}
 		i++;
 	}
-	i = 0;
-	while (i < data->number_of_philosophers)
-	{
+	while (--i >= 0)
 		pthread_join(data->philosophers[i].thread, NULL);
-		i++;
-	}
 }
